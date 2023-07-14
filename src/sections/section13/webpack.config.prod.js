571854854935ac629a0dc6faf114c2bd61093900
Fernanda Gonzalez <fernandagch@ciencias.unam.mx>
2023-07-13 19:02:53 -0600

    Session 13: 168-175 Practice: Select & Share Place App

--- src/sections/section13/webpack.config.prod.js
+++ src/sections/section13/webpack.config.prod.js
@@ -1,15 +1,9 @@
 const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/app.ts',
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -26,5 +20,8 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
 };
